bb3a9eea994f0f9a0b725b789d752ea412928126
Konstantin Reichmeyer <kounst@gmx.de>
2012-02-12 17:46:35 +0100

    two step positioning for startup

--- canBLC/source/inc/startup.h
+++ canBLC/source/inc/startup.h
@@ -3,10 +3,10 @@
 // Author: Konstantin Reichmeyer
 // Date: 28.04.10
 
-#define POSITIONING_FORCE      60
-#define START_PUSH_FORCE      200
-#define START_RAMP_FORCE      200
-#define RAMP_INITIAL_PERIOD 30000
+#define POSITIONING_FORCE      50
+#define START_PUSH_FORCE       100
+#define START_RAMP_FORCE       120          //has to be adapted to supply voltage
+#define RAMP_INITIAL_PERIOD 40000
 void forced_start_push  (void);  //mode 0
 void forced_start_ramp  (void);  //mode 1
 void positioning        (void);
@@ -14,5 +14,5 @@ void positioning        (void);
 
 
 
-void startup            (uint8_t mode);
+uint8_t startup            (uint8_t mode);
 
