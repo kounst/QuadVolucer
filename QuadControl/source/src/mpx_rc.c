c5a20dacfc3827281dbaae8d9e85d76b6ef32424
Konstantin Reichmeyer <kounst@gmx.de>
2012-03-22 18:00:40 +0100

    comments added

--- QuadControl/source/src/mpx_rc.c
+++ QuadControl/source/src/mpx_rc.c
@@ -3,27 +3,36 @@
 // Author:    Konstantin Reichmeyer
 // Date:      18.03.2012
 
+/*
+    Multiplex M-LINK receivers (e.g. RX-7-DR) can be configured to provide digital servo data over a UART interface.
+    mpx_rc.h and mpx_rc.c is an implementation of that interface. 
+    
+    The UART parameters are 115200 Baud, 8-N-1 
+*/
+
 
 #include "stm32f10x.h"
 #include "main.h"
 #include "mpx_rc.h"
 
-volatile uint8_t sync_gap = 0;
+volatile uint8_t sync_gap = 0;                                      //gets increased by Sys-Tick 
 uint8_t rx_data[27];
 extern union pulsw pulswidth;
 
-extern uint8_t NoofByte;
-extern char *TxBuf;
-
-uint16_t crc_xmodem_update(uint16_t, uint8_t);
 
 
+
+/**
+  * @brief  Parses servo data from series of data bytes sent by MPX receiver
+  * @param  data byte sent by MPX receiver
+  * @retval None
+  */
 void mpx_parse_data(uint8_t rx_byte)
 {
   static uint8_t bytecount = 0;
   static uint16_t crc_ccitt = 0;
 
-  if((sync_gap > 10) && (rx_byte == 0xA1))
+  if((sync_gap > 10) && (rx_byte == 0xA1))                          //find start of data-package
   {  
     bytecount = 0;
     crc_ccitt = 0;
@@ -34,7 +43,7 @@ void mpx_parse_data(uint8_t rx_byte)
   rx_data[bytecount] = rx_byte;
   
   
-  if(bytecount == 26 && crc_ccitt == 0)
+  if(bytecount == 26 && crc_ccitt == 0)                             //if end of data-package && crc sum is correct
   {
     pulswidth.pw[0] = ((uint16_t)rx_data[1]  << 8) | rx_data[2];
     pulswidth.pw[1] = ((uint16_t)rx_data[3]  << 8) | rx_data[4];
@@ -48,7 +57,13 @@ void mpx_parse_data(uint8_t rx_byte)
     bytecount++;
 }
 
-uint16_t crc_xmodem_update(uint16_t crc, uint8_t data)
+
+/**
+  * @brief  CRC-XMODEM calculation
+  * @param  CRC, DATA
+  * @retval updated CRC
+  */
+uint16_t crc_xmodem_update(uint16_t crc, uint8_t data)              
 {
   int i;
 
