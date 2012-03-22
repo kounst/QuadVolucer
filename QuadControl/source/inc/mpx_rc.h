2e058eef826d0d8515734262890a057e2f4cf1d6
Konstantin Reichmeyer <kounst@gmx.de>
2012-03-22 18:00:05 +0100

    comments added

--- QuadControl/source/inc/mpx_rc.h
+++ QuadControl/source/inc/mpx_rc.h
@@ -3,8 +3,13 @@
 // Author:    Konstantin Reichmeyer
 // Date:      18.03.2012
 
+/*
+    Multiplex M-LINK receivers (e.g. RX-7-DR) can be configured to provide digital servo data over a UART interface.
+    mpx_rc.h and mpx_rc.c is an implementation of that interface. 
+    
+    The UART parameters are 115200 Baud, 8-N-1 
+*/
+
 
 void mpx_parse_data (uint8_t); 
-void crc_ccitt_update(unsigned char);
-unsigned int crc_ccitt_crc(void);
-void crc_ccitt_init(void);
+uint16_t crc_xmodem_update(uint16_t, uint8_t);
